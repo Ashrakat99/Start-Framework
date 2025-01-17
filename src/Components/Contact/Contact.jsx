af151def2a89e427f4e7fad373a944ae712808db
Ashrakat Mohammed <ashrakatmohammed1234@gmail.com>
2025-01-18 00:46:07 +0200

    update

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 export default function Contact() {
   return (
     <div>
-      <div className="pt-4 text-center pt-24">
-        <h2 className="text-4xl font-extrabold my-10 text-[#2c3e50] text-uppercase">Contact Component</h2>
+      <div className="pt-4 text-center ">
+        <h2 className="text-4xl font-extrabold my-10 text-[#2c3e50] text-uppercase pt-16">Contact Component</h2>
         <div className="flex items-center justify-center mb-3">
           <div className="line bg-[#2c3e50] w-16 h-1 mx-3"></div>
           <i className="fa-solid fa-star text-2xl text-[#2c3e50]"></i>
